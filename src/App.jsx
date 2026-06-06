0f7e3ddde1afd31296cc6b9c2e26dc9ed6f9f197
IRENE <irenerutto@gmail.com>
2026-06-06 10:17:06 +0300

    first commit react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
-import ProductList from './components/ProductList'
 import DarkModeToggle from './components/DarkModeToggle'
 import Cart from './components/Cart'
+import ProductList, { sampleProducts } from './components/ProductList'
 
 const App = () => {
   // TODO: Implement state for dark mode toggle
+   const [darkMode, setDarkMode] = useState(false)
 
   // TODO: Implement state for cart management
-
+  const [cart, setCart] = useState([])
   // TODO: Implement state for category filtering
-
+  const [category, setCategory] = useState('all')
+ const addToCart = (product) => {
+  setCart([...cart, product])
+}
+   const filteredProducts =
+    category === "all"
+      ? sampleProducts
+      : sampleProducts.filter((p) => p.category === category);
   return (
     <div>
       <h1>🛒 Shopping App</h1>
@@ -19,18 +27,31 @@ const App = () => {
       </p>
 
       {/* TODO: Render DarkModeToggle and implement dark mode functionality */}
+       <DarkModeToggle
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+      />
 
       {/* TODO: Implement category filter dropdown */}
       <label>Filter by Category: </label>
-      <select>
+      <select
+      value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      
+      >
         <option value="all">All</option>
         <option value="Fruits">Fruits</option>
         <option value="Dairy">Dairy</option>
       </select>
 
-      <ProductList />
+      <ProductList 
+       sampleProducts={sampleProducts}
+       category={category}
+      addToCart={addToCart}
+      />
 
       {/* TODO: Implement and render Cart component */}
+       <Cart cart={cart} />
     </div>
   )
 }
